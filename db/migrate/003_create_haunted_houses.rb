6f7e85cbc69f18361f1a30d4f1676c342bdbf9f6
MarcusMellorMIM <marcus@mellor.com>
2019-05-03 08:41:36 +0100

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+# Create your costume_stores migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :haunted_houses do |t|
+        t.string  :name
+        t.string  :location
+        t.boolean :family_friendly
+        t.string  :theme
+        t.float   :price
+        t.datetime  :opening_date
+        t.datetime  :closing_date
+        t.text    :description
+        t.timestamps
+    end
+  end
+end
